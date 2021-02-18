1151f30665610775798f2bd8c4bb98056feffc09
lyxell <alyxell@kth.se>
2021-02-18 11:49:32 +0100

    Clean up

--- file.cpp
+++ file.cpp
@@ -7,17 +7,12 @@
 #include "utf8.h"
 
 buffer file_load(const char* filename) {
-    buffer b = {
-        {std::make_shared<buffer_line>()},
-        {0, 0}
-    };
     std::ifstream t(filename);
     std::string str((std::istreambuf_iterator<char>(t)),
                      std::istreambuf_iterator<char>());
-    auto u32s = utf8_decode(str);
     buffer_lines lines;
     buffer_line line;
-    for (char32_t c : u32s) {
+    for (char32_t c : utf8_decode(str)) {
         if (c == '\n') {
             lines.push_back(std::make_shared<buffer_line>(std::move(line)));
             line.clear();
