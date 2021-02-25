46f37bb3e3b174af225ddbbaa1d4f7747bba052a
lyxell <alyxell@kth.se>
2021-02-25 02:25:58 +0100

    Add I movement

--- editor.cpp
+++ editor.cpp
@@ -54,13 +54,17 @@ static editor editor_handle_command_normal(editor e) {
         e.buf = buffer_move_end_of_line(std::move(e.buf));
         return e;
     }
-    "A" {
+    "I" | "A" {
         e.cmd = {};
         e.mode = MODE_INSERT;
         if (!e.history.size() || e.history.back().lines != e.buf.lines) {
             e.history.push_back(e.buf);
         }
-        e.buf = buffer_move_end_of_line(std::move(e.buf));
+        if (yych == 'A') {
+            e.buf = buffer_move_end_of_line(std::move(e.buf));
+        } else {
+            e.buf = buffer_move_start_of_line(std::move(e.buf));
+        }
         return e;
     }
     "G" {
