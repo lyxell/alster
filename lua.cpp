a46d6f03fb9c4f26fbaa665188e73d823bb5462b
lyxell <alyxell@kth.se>
2021-02-27 17:42:05 +0100

    Clean up

--- lua.cpp
+++ lua.cpp
@@ -2,113 +2,108 @@
 #include "lua.h"
 #include <cassert>
 
-int lua_line_sub(lua_State *L) {
-    assert(lua_gettop(L) == 3); /* number of arguments */
-    assert(lua_isnumber(L, -1));
-    assert(lua_isnumber(L, -2));
-    long int from = lua_tointeger(L, -2) - 1; // lua 1-indexed
-    long int to = lua_tointeger(L, -1) - 1; // lua 1-indexed
-    size_t len = to - from + 1;
-    assert(len >= 0);
-    assert(lua_isuserdata(L, -3));
-    buffer_char* line = (buffer_char*) lua_touserdata(L, -3);
-    buffer_char* subline = (buffer_char*) lua_newuserdata(L, sizeof(buffer_char) * len + 1);
-    std::copy(line + 1 + from, line + 1 + from + len, subline + 1);
-    subline[0] = (buffer_char) len;
+template<typename It>
+static void create_line(lua_State *L, It start, It end) {
+    size_t length = end - start;
+    size_t num_bytes = sizeof(buffer_char) * (length + 1);
+    auto line = (buffer_char*) lua_newuserdata(L, num_bytes);
+    line[0] = (buffer_char) length;
+    std::copy(start, end, line + 1);
     lua_newtable(L);
     lua_pushstring(L, "__len");
     lua_pushcfunction(L, lua_line_len);
     lua_settable(L, -3);
     lua_setmetatable(L, -2);
+}
+
+int lua_line_sub(lua_State *L) {
+    buffer_char *line;
+    long start;
+    long end;
+    if (lua_gettop(L) == 3) {
+        assert(lua_isnumber(L, -1));
+        assert(lua_isnumber(L, -2));
+        assert(lua_isuserdata(L, -3));
+        line = (buffer_char*) lua_touserdata(L, -3);
+        start = lua_tointeger(L, -2) - 1;
+        end = lua_tointeger(L, -1) - 1;
+    } else {
+        assert(lua_isnumber(L, -1));
+        assert(lua_isuserdata(L, -2));
+        line = (buffer_char*) lua_touserdata(L, -2);
+        start = lua_tointeger(L, -1) - 1;
+        end = line[0];
+    }
+    create_line(L, line + 1 + start, line + 1 + end);
     return 1; 
 }
 
 int lua_line_create(lua_State *L) {
-    buffer_char* line = (buffer_char*) lua_newuserdata(L, sizeof(buffer_char));
-    line[0] = 0;
-    lua_newtable(L);
-    lua_pushstring(L, "__len");
-    lua_pushcfunction(L, lua_line_len);
-    lua_settable(L, -3);
-    lua_setmetatable(L, -2);
+    buffer_char* p = 0;
+    create_line(L, p, p);
     return 1; 
 }
 
 int lua_lines_insert(lua_State *L) {
-    assert(lua_gettop(L) == 3); /* number of arguments */
-    assert(lua_isuserdata(L, -1)); // value
-    assert(lua_isnumber(L, -2)); // pos
-    assert(lua_isuserdata(L, -3)); // lines
-    buffer_char* line = (buffer_char*) lua_touserdata(L, -1);
-    long int pos = lua_tointeger(L, -2) - 1;
-    buffer_lines& lines = **((buffer_lines**) lua_touserdata(L, -3));
-    lines.insert(lines.begin() + pos, std::make_shared<buffer_line>(
+    assert(lua_gettop(L) == 3);
+    assert(lua_isuserdata(L, -1));
+    assert(lua_isnumber(L, -2));
+    assert(lua_isuserdata(L, -3));
+    auto line = (buffer_char*) lua_touserdata(L, -1);
+    auto line_index = lua_tointeger(L, -2) - 1;
+    auto& lines = **((buffer_lines**) lua_touserdata(L, -3));
+    lines.insert(lines.begin() + line_index, std::make_shared<buffer_line>(
         buffer_line(line + 1, line[0])));
     return 0; /* number of results */
 }
 
 int lua_lines_remove(lua_State *L) {
-    assert(lua_gettop(L) == 2); /* number of arguments */
-    assert(lua_isnumber(L, -1)); // pos
-    assert(lua_isuserdata(L, -2)); // lines
-    long int pos = lua_tointeger(L, -1) - 1;
-    buffer_lines& lines = **((buffer_lines**) lua_touserdata(L, -2));
-    lines.erase(lines.begin() + pos);
-    return 0; /* number of results */
+    assert(lua_gettop(L) == 2);
+    assert(lua_isnumber(L, -1));
+    assert(lua_isuserdata(L, -2));
+    auto line_index = lua_tointeger(L, -1) - 1;
+    auto& lines = **((buffer_lines**) lua_touserdata(L, -2));
+    lines.erase(lines.begin() + line_index);
+    return 0;
 }
 
 int lua_lines_len(lua_State *L) {
-    // TODO: Why is this value 2? Shouldn't it be 1?
-    assert(lua_gettop(L) == 2); /* number of arguments */
+    assert(lua_gettop(L) == 2);
+    assert(lua_isnil(L, -1));
     assert(lua_isuserdata(L, -2));
-    buffer_lines** lines = (buffer_lines**) lua_touserdata(L, -2);
-    lua_pushnumber(L, (int) (*lines)->size());
-    return 1; /* number of results */
+    auto& lines = **((buffer_lines**) lua_touserdata(L, -2));
+    lua_pushinteger(L, (int) lines.size());
+    return 1;
 }
 
 int lua_line_len(lua_State *L) {
-    // TODO: Why is this value 2? Shouldn't it be 1?
-    assert(lua_gettop(L) == 2); /* number of arguments */
+    assert(lua_gettop(L) == 2);
+    assert(lua_isnil(L, -1));
     assert(lua_isuserdata(L, -2));
-    buffer_char* line = (buffer_char*) lua_touserdata(L, -2);
-    lua_pushnumber(L, (int) line[0]);
-    return 1; /* number of results */
+    auto line = (buffer_char*) lua_touserdata(L, -2);
+    lua_pushinteger(L, (int) line[0]);
+    return 1;
 }
 
 int lua_lines_newindex(lua_State *L) {
-    assert(lua_gettop(L) == 3); /* number of arguments */
-    assert(lua_isuserdata(L, -1)); // value
+    assert(lua_gettop(L) == 3);     // number of arguments
+    assert(lua_isuserdata(L, -1));  // value
     assert(lua_isnumber(L, -2)); // key
     assert(lua_isuserdata(L, -3)); // table
-    // get the actual line index
-    long int idx = lua_tointeger(L, -2) - 1; // subtract 1, lua is 1-indexed
-    // extract the pointer to the lines from the user data
-    buffer_lines& lines = **((buffer_lines**) lua_touserdata(L, -3));
-    // extract the pointer to the string in memory
-    buffer_char* mem = (buffer_char*) lua_touserdata(L, -1);
-    lines[idx] = std::make_shared<buffer_line>(mem + 1, mem[0]);
+    auto line = (buffer_char*) lua_touserdata(L, -1);
+    auto line_index = lua_tointeger(L, -2) - 1;
+    auto& lines = **((buffer_lines**) lua_touserdata(L, -3));
+    lines[line_index] = std::make_shared<buffer_line>(line + 1, line[0]);
     return 0;
 }
 
 int lua_lines_index(lua_State *L) {
-    assert(lua_gettop(L) == 2); /* number of arguments */
+    assert(lua_gettop(L) == 2);
     assert(lua_isnumber(L, -1));
     assert(lua_isuserdata(L, -2));
-    // get the actual line index
-    long int idx = lua_tointeger(L, -1) - 1; // subtract 1, lua is 1-indexed
-    // extract the pointer to the lines from the user data
-    buffer_lines& lines = **((buffer_lines**) lua_touserdata(L, -2));
-    // create line and metatable
-    buffer_line& line = *lines[idx];
-    // allocate space for the string and one extra slot for size
-    buffer_char* mem = (buffer_char*) lua_newuserdata(L, sizeof(buffer_char) * line.size() + 1);
-    std::copy(line.begin(), line.end(), mem + 1);
-    mem[0] = (buffer_char) line.size();
-    lua_newtable(L);
-    lua_pushstring(L, "__len");
-    lua_pushcfunction(L, lua_line_len);
-    lua_settable(L, -3);
-    lua_setmetatable(L, -2);
-    return 1; /* number of results */
+    long int line_index = lua_tointeger(L, -1) - 1;
+    auto& lines = **((buffer_lines**) lua_touserdata(L, -2));
+    create_line(L, lines[line_index]->begin(), lines[line_index]->end());
+    return 1;
 }
 
